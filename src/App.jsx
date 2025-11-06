257b6c31d58292a154d9d28e044e05f9e2587ad2
9diin <weartstudio@naver.com>
2025-11-06 15:51:25 +0900

    [feature] AppHeader 컴포넌트 UI 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Heart } from "lucide-react";
 import AppHeader from "./components/common/AppHeader";
+import { Separator } from "./components/ui/separator";
 
 function App() {
     return (
         <div className="w-full h-screen">
             {/* 배너 */}
-            <div></div>
+            <div className="w-full h-15 flex items-center justify-center bg-[#4EABFF] text-xl font-semibold">
+                <p className="text-white">레퍼런스로 시작하는 스몰 브랜드 브랜딩 워크숍</p>
+            </div>
             {/* 자회사 링크 */}
-            <div></div>
+            <div className="w-full h-[34px] flex items-center gap-3 px-8">
+                <img src="" alt="" />
+                <Separator orientation="vertical" className="h-3!" />
+                <p className="text-sm font-medium">notefolio</p>
+                <Separator orientation="vertical" className="h-3!" />
+                <p className="text-sm text-neutral-400 font-bold">LOUD</p>
+            </div>
             {/* 헤더 */}
             <AppHeader />
         </div>
--- src/components/common/AppHeader.jsx
+++ src/components/common/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react";
+import { ChevronDown, Search } from "lucide-react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
@@ -6,43 +6,54 @@ const menu = [
     {
         label: "발견",
         newest: false,
+        dropdown: false,
+        underline: true,
     },
     {
         label: "채용",
         newest: true,
+        dropdown: false,
+        underline: false,
     },
     {
         label: "워크숍/커뮤니티",
         newest: false,
+        dropdown: true,
+        underline: false,
     },
     {
         label: "포폴 피드백",
         newest: true,
+        dropdown: false,
+        underline: false,
     },
     {
         label: "에이전시",
         newest: false,
+        dropdown: false,
+        underline: false,
     },
 ];
 
 function AppHeader() {
     return (
-        <header className="w-full flex items-center justify-between py-2 px-8 border-b">
-            <div className="flex items-center gap-10">
+        <header className="w-full h-14 flex items-center justify-between px-8 border-b">
+            <div className="h-full flex items-center gap-10">
                 {/* 로고 */}
                 <img src="src/assets/logo.svg" alt="@LOGO" className="w-30" />
-                <nav className="flex items-center gap-4">
+                <nav className="h-full flex items-center gap-4">
                     {menu.map((item) => {
                         return (
-                            <div className="flex items-center gap-1 font-medium">
-                                {item.label}
+                            <div className={`h-full flex items-center gap-1 font-medium ${item.underline && "h-[calc(100%-2px)] border-b-2 border-black"}`}>
+                                <p className={`text-[15px] ${item.underline && "mt-0.5"}`}>{item.label}</p>
+                                {item.dropdown && <ChevronDown size={16} />}
                                 {item.newest && <p className="text-xs text-[#05BCC6] font-medium">NEW</p>}
                             </div>
                         );
                     })}
                 </nav>
             </div>
-            <div class="flex items-center gap-2">
+            <div className="flex items-center gap-2">
                 <div className="flex items-center border px-3 rounded-full bg-neutral-50">
                     <Search size={18} className="text-neutral-400" />
                     <Input placeholder="230,000개 이상의 크리에이티브 검색" className="w-60 placeholder:text-neutral-400 outline-0 border-none focus-visible:ring-0" />
