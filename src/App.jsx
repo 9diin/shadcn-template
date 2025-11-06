bb22a0acc03b4e8ae0b3a31cca8f09375759dd49
9diin <weartstudio@naver.com>
2025-11-06 13:28:53 +0900

    [chore] Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Search } from "lucide-react";
+import { Input } from "./components/ui/input";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="w-full h-screen">
+            {/* 배너 */}
+            <div></div>
+            {/* 자회사 링크 */}
+            <div></div>
+            {/* 헤더 */}
+            <header className="w-full flex items-center justify-between">
+                <div></div>
+                <div>
+                    {/* 검색창 */}
+                    <div className="flex items-center gap-2">
+                        <Search size={18} />
+                        <Input placeholder="230,000개 이상의 크리에이티브 검색" />
+                    </div>
+                    {/* 로그인 */}
+                    {/* 회원가입 */}
+                </div>
+            </header>
+        </div>
+    );
 }
 
-export default App
+export default App;
