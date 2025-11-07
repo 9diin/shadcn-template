8f5f70b333e504ddb0863ee209db7ec32c9a0217
9diin <weartstudio@naver.com>
2025-11-07 12:06:08 +0900

    [feature] 검색 필터링 UI 및 이미지 리스트 스켈레톤 UI 작업 完

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,83 @@
+import { ArrowUpDown, Brush, Camera, CirclePlay, Eye, Gem, Heart, IdCard, MousePointerClick, Package, Palette, Panda, PenTool, Sparkles, Type } from "lucide-react";
 import AppHeader from "./components/common/AppHeader";
 import { Separator } from "./components/ui/separator";
 import { Skeleton } from "./components/ui/skeleton";
+import SkeletonImageCard from "./components/skeleton/ImageCard";
 
 function App() {
     const gallery = ["", "", "", "", "", "", ""];
+    const categories = [
+        {
+            icon: Camera,
+            label: "전체",
+            isActive: true, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: CirclePlay,
+            label: "영상/모션그래픽",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Palette,
+            label: "그래픽 디자인",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: IdCard,
+            label: "브랜딩/편집",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: MousePointerClick,
+            label: "UI/UX",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: PenTool,
+            label: "일러스트레이션",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Camera,
+            label: "디지털 아트",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Sparkles,
+            label: "AI",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Panda,
+            label: "캐릭터 디자인",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Package,
+            label: "제품/패키지 디자인",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Camera,
+            label: "포토그래피",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Type,
+            label: "타이포그래피",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Gem,
+            label: "공예",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+        {
+            icon: Brush,
+            label: "파인아트",
+            isActive: false, // 라벨 색상 통제를 위한 속성
+        },
+    ];
 
     return (
         <div className="w-full h-screen">
@@ -13,7 +87,7 @@ function App() {
             </div>
             {/* 자회사 링크 */}
             <div className="w-full h-[34px] flex items-center gap-3 px-8">
-                <img src="" alt="" />
+                {/* <img src="" alt="" /> */}
                 <Separator orientation="vertical" className="h-3!" />
                 <p className="text-sm font-medium">notefolio</p>
                 <Separator orientation="vertical" className="h-3!" />
@@ -21,19 +95,47 @@ function App() {
             </div>
             {/* 헤더 */}
             <AppHeader />
-            <main className="py-6">
+            <main className="flex flex-col items-center py-6">
                 {/* 메인 홍보 갤러리 */}
                 <section className="flex items-center gap-6 overflow-x-scroll">
                     {gallery.map(() => (
                         <Skeleton className="min-w-[520px] w-[520px] h-80" />
                     ))}
-                    {/* <Skeleton className="min-w-[520px] w-[520px] h-80" />
-                    <Skeleton className="min-w-[520px] w-[520px] h-80" />
-                    <Skeleton className="min-w-[520px] w-[520px] h-80" />
-                    <Skeleton className="min-w-[520px] w-[520px] h-80" />
-                    <Skeleton className="min-w-[520px] w-[520px] h-80" />
-                    <Skeleton className="min-w-[520px] w-[520px] h-80" />
-                    <Skeleton className="min-w-[520px] w-[520px] h-80" /> */}
+                </section>
+                {/* STIKCY MENU */}
+                <section className="flex gap-10 mt-20">
+                    <div className="flex flex-col gap-2">
+                        {/* 아이콘 */}
+                        <ArrowUpDown className="text-neutral-700" />
+                        {/* 아이콘 라벨 */}
+                        <p className="text-sm">정렬</p>
+                    </div>
+                    <Separator orientation="vertical" className="h-10!" />
+                    <div className="flex items-center gap-12">
+                        {categories.map((category) => {
+                            const IconComponent = category.icon;
+
+                            return (
+                                <div className="flex flex-col items-center gap-2">
+                                    <IconComponent className="text-neutral-700" />
+                                    <p className="text-sm">{category.label}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </section>
+                {/* IMAGE LIST */}
+                <section className="w-full grid grid-cols-6 gap-6 mt-6 px-20">
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
+                    <SkeletonImageCard />
                 </section>
             </main>
         </div>
